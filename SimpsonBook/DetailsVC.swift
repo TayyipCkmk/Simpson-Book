53af26f7dc54f5f9e779dfea4a10dfcf81a32264
Tayyip Çakmak <tayyipcakmak@hotmail.com.tr>
2020-10-18 16:38:47 +0300

    first commit

---
+++ SimpsonBook/DetailsVC.swift
@@ -0,0 +1,25 @@
+//
+//  DetailsVC.swift
+//  SimpsonBook
+//
+//  Created by Tayyip Çakmak on 18.10.2020.
+//  Copyright © 2020 Tayyip Cakmak. All rights reserved.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
