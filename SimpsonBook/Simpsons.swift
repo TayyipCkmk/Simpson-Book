5eec248c7978cc34527b56c151369c099558fb26
Tayyip Çakmak <tayyipcakmak@hotmail.com.tr>
2020-10-18 17:27:08 +0300

    second commit

---
+++ SimpsonBook/Simpsons.swift
@@ -0,0 +1,9 @@
+//
+//  Simpsons.swift
+//  SimpsonBook
+//
+//  Created by Tayyip Çakmak on 18.10.2020.
+//  Copyright © 2020 Tayyip Cakmak. All rights reserved.
+//
+
+import Foundation
--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      tableView.dataSource = self
+      tableView.delegate = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Homer"
+        return cell
+    }
 
 }
 
