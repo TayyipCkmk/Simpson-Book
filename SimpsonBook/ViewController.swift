aa7bd101e3062f905c822ab7dfb0be81708b13d7
Tayyip Çakmak <tayyipcakmak@hotmail.com.tr>
2020-10-18 16:20:14 +0300

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Tayyip Çakmak on 18.10.2020.
+//  Copyright © 2020 Tayyip Cakmak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
